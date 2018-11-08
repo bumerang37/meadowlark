bbcd7de5d1753db03689472cdd410ea60406d84e
bumerang37 <bumerank@mail.ru>
2018-11-08 08:44:08 +0500

    Added tests

--- meadowlark.js
+++ meadowlark.js
@@ -1,14 +1,34 @@
+//
 var fortune = require('./lib/fortune.js');
 var express = require('express');
 
 var app = express();
 
+
+
+
 // Установка механизма представления handlebars
 var handlebars = require('express-handlebars')
-    .create({ defaultLayout: 'main'});
+    .create({ defaultLayout: 'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+} }
+});
 app.engine('handlebars', handlebars.engine);
 
 app.set('view engine', 'handlebars');
+
+//Tests
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && 
+    req.query.test === '1';
+    next();
+});
+//Здесь находятся маршрутры
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
@@ -20,7 +40,20 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', { fortune: fortune.getFortune()});
+    res.render('about', { 
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 // Обобщенный обработчик 404 (промежуточное ПО)
@@ -40,3 +73,5 @@ app.listen(app.get('port'), function() {
     console.log( 'Express запущен на http://localhost:' +
     app.get('port') + ': нажмите Ctrl+C для завершения.');
 });
+
+if( app.thing == null ) console.log( 'Бе-е!' );
