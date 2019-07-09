22796d5c130870b776c7d89c1ed28c0a423f6f66
bumerang37 <bumerank@mail.ru>
2019-07-10 00:34:27 +0500

    Edited newsletters form and form parser

--- meadowlark.js
+++ meadowlark.js
@@ -4,9 +4,10 @@ var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
 
+
 var app = express();
 
-
+var credentials = require('./credentials.js');
 
 
 // Установка механизма представления handlebars
@@ -40,6 +41,14 @@ app.use(require('body-parser').urlencoded({
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
+
+
 app.use(function (req, res, next) {
     if (!res.locals.partials) res.locals.partials = {};
     res.locals.partials.weatherContext = getWeatherData();
@@ -48,7 +57,6 @@ app.use(function (req, res, next) {
 
 // jQuery File Upload endpoint middleware
 app.use('/upload', function(req, res, next){
-    console.log('test');
     var now = Date.now();
    
     jqupload.fileHandler({
@@ -62,6 +70,13 @@ app.use('/upload', function(req, res, next){
     })(req, res, next);
 });
 
+app.use(function(req, res, next) {
+    //Если имеется экстренное сообщение, переместим его в контекст
+    //Затем удалим
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -104,13 +119,48 @@ app.get('/data/nursery-rhyme', function (req, res) {
 
 app.get('/thank-you', function (req, res) {
     res.render('thank-you');
-})
+});
 
-app.get('/newsletter', function (req, res) {
-    //Позже здесь будет CSRF, пока заглушка
-    res.render('newsletter', {
-        csrf: 'CSRF token goes here'
-    });
+app.get('/newsletter', function(req, res){
+	res.render('newsletter');
+});
+
+var VALID_EMAIL_REGEX = new RegExp('^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@' +    '[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?' +    '(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$');
+
+
+app.post('/newsletter', function(req, res){
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) {
+		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+		req.session.flash = {
+			type: 'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was  not valid.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	}
+	new NewsletterSignup({ name: name, email: email }).save(function(err){
+		if(err) {
+			if(req.xhr) return res.json({ error: 'Database error.' });
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.',
+			};
+			return res.redirect(303, '/newsletter/archive');
+		}
+		if(req.xhr) return res.json({ success: true });
+		req.session.flash = {
+			type: 'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	});
+});
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
 });
 
 app.post('/process', function (req, res) {
